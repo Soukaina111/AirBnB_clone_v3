3d8f34cb1dbf7555cd89c65ae87b132e060890b7
SaidLamghari <said.lmg@gmail.com>
2024-04-30 15:15:41 -0700

    api/v1/views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""initialise le package"""
+
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# package Ajouté 7. City
+# Importer les modules de niveau supérieur au début du fichier
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-# 8. Amenity
 from api.v1.views.amenities import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')²
