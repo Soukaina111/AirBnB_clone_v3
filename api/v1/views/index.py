9721017b4ab4f6deeaa0655ba9a672a9b912e0ed
SaidLamghari <said.lmg@gmail.com>
2024-04-30 17:34:05 -0700

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -23,19 +23,6 @@ def get_statistics():
     Endpoint to retrieve statistics about different entities in storage.
     Returns a JSON response with counts for each entity.
     """
-    entity_counts = {
-        'states': storage.count(State),
-        'users': storage.count(User),
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review)
-    }
-    return jsonify(entity_counts)
-
-@app_views.route('/stats')
-def stats():
-    """ """
     listskv = {"amenities": "Amenity",
                "cities": "City",
                "places": "Place",
