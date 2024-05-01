2136d87ffec5963937c39600b8299ca40ecda96c
SaidLamghari <said.lmg@gmail.com>
2024-05-01 14:39:10 -0700

    remove a test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''let's configure flask status'''
 from flask import jsonify
-"""
 from api.v1.views import app_views
 from models.amenity import Amenity
 from models.city import City
@@ -10,14 +9,14 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from models import storage
-"""
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     ''' shows the status'''
     return jsonify({'status': 'OK'})
 
-"""
+
 @app_views.route('/stats', strict_slashes=False)
 def get_statistics():
     """
@@ -32,4 +31,4 @@ def get_statistics():
         'places': storage.count(Place),
         'reviews': storage.count(Review)
     }
-    return jsonify(entity_counts)"""
+    return jsonify(entity_counts)
