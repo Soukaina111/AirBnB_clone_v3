12de7c38d54e85d800359031babe54d65f4de297
SaidLamghari <said.lmg@gmail.com>
2024-05-02 16:37:16 -0700

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" let's configure a flask app
-"""
-
+""" let's configure a flask app"""
 from flask import Flask, jsonify
 from flask import abort, request
 from flask_cors import CORS
