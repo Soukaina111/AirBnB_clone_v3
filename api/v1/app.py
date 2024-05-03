2d5a86e9652582a6349a584b6b05a0c70d294db0
SaidLamghari <said.lmg@gmail.com>
2024-05-03 01:19:21 -0700

    api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, jsonify
 from flask import abort, request
-from flask_cors import CORS
+# from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
